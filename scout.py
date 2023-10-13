27336af4832683baadac1ed6665338e9013c6381
Jason Saini <jasonsaini.exe@gmail.com>
2023-10-13 13:32:16 -0400

    add logic for unique identifiers and secrets for token request

--- scout.py
+++ scout.py
@@ -7,3 +7,22 @@ load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+client_credentials_base64 = base64.b64encode(client_credentials.encode())
+
+token_url = 'https://accounts.spotify.com/api/token'
+headers = {
+    'Authorization': f'Basic {client_credentials_base64.decode()}'
+}
+data = {
+    'grant_type': 'client_credentials'
+}
+response = requests.post(token_url, data=data, headers=headers)
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+    print("Access token obtained successfully.")
+else:
+    print("Error obtaining access token.")
+    exit()
