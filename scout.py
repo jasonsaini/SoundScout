9d2e3109a90667db25a6dd0a8505370fe9c1bad4
Jason Saini <jasonsaini.exe@gmail.com>
2023-10-13 13:34:08 -0400

    add search artist functionality

--- scout.py
+++ scout.py
@@ -10,19 +10,43 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
 client_credentials_base64 = base64.b64encode(client_credentials.encode())
+def get_token():
+    # Request the access token
+    token_url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': f'Basic {client_credentials_base64.decode()}'
+    }
+    data = {
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(token_url, data=data, headers=headers)
 
-token_url = 'https://accounts.spotify.com/api/token'
-headers = {
-    'Authorization': f'Basic {client_credentials_base64.decode()}'
-}
-data = {
-    'grant_type': 'client_credentials'
-}
-response = requests.post(token_url, data=data, headers=headers)
+    if response.status_code == 200:
+        access_token = response.json()['access_token']
+        print("Access token obtained successfully.")
+        return access_token
+    else:
+        print("Error obtaining access token.")
+        exit()
 
-if response.status_code == 200:
-    access_token = response.json()['access_token']
-    print("Access token obtained successfully.")
-else:
-    print("Error obtaining access token.")
-    exit()
+
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    # get first artist that appears in the search
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = requests.get(query_url, headers=headers)
+    json_result = result.json()
+    print(json_result)
+
+
+token = get_token()
+
+search_for_artist(token, "khalid")
